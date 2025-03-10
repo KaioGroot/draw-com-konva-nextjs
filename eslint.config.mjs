0f42892cca53ac078426dec46189ad0c8a036348
KaioGroot <85202894+KaioGroot@users.noreply.github.com>
2025-03-09 21:18:16 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
-const eslintConfig = {
-    extends: ["next/core-web-vitals"],
-    env: {
-        node: true
-    }
-};
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals") // Converte automaticamente o formato antigo
+];
+
 export default eslintConfig;
