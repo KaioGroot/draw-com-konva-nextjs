649c9d00d3bc9f5e0be5821daae681f8cb21ebd8
unknown <kaiomagalhaesxp@gmail.com>
2025-03-09 22:06:08 -0400

    melhorias

--- src/app/page.js
+++ src/app/page.js
@@ -158,7 +158,6 @@ export default function Home() {
                                 <path d="M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z" />
                                 <path d="m15 5 3 3" />
                             </svg>
-
                         )}
                     </button>
                 </div>
@@ -195,7 +194,7 @@ export default function Home() {
                                 width: '100%',
                                 appearance: 'none',
                                 background: 'linear-gradient(to right, #333 0%, #333 100%)',
-                                backgroundSize: `${(brushSize * parent.innerWidth) / 180}px 100%`,
+                                backgroundSize: `${brushSize / 18}px 100%`,
                                 backgroundRepeat: 'no-repeat',
                                 borderRadius: '10px',
                                 padding: '0.5rem',
